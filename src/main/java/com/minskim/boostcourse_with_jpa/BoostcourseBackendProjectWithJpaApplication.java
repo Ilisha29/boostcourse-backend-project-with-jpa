8ae01255ad5b1ae575613916f7dd5d11ee83f02d
Ilisha29 <94konzern@gmail.com>
2021-06-25 18:22:52 +0900

    Initial commit

---
+++ src/main/java/com/minskim/boostcourse_with_jpa/BoostcourseBackendProjectWithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.minskim.boostcourse_with_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoostcourseBackendProjectWithJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoostcourseBackendProjectWithJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minskim/boostcourse_with_jpa/BoostcourseBackendProjectWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minskim.boostcourse_with_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoostcourseBackendProjectWithJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
